679044eb39a169764232c6102b273ec75e339aa8
Menno007 <Menna.soft3@gmail.com>
2023-11-28 14:43:49 +0200

    1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,8 @@ number = random.randint(-10000, 10000)
 if number < 0:
     number = number * -1
     last_digit = (number % 10) * -1
-elif number >= 0:
+    number = number * -1
+elif number > 0:
     last_digit = number % 10
 
 if last_digit > 5:
