84f287d8c65c96b84916678ce7bef9e3582bb518
Menno007 <Menna.soft3@gmail.com>
2023-11-29 02:28:21 +0200

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 for c in reversed(range(97, 123)):
     print("{:c}".format(c) if c % 2 == 0 else "{:c}".format(c - 32), end='')
+"""
+    i = 0
+    for c in range(ord('z'), ord('a') - 1, -1):
+        print("{}".format(chr(c - i)), end="")
+        i = 32 if i == 0 else 0
+"""
