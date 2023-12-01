00060a6e510281e5b7b78d5a8b2221249de4ecdf
Menno007 <Menna.soft3@gmail.com>
2023-12-01 03:19:19 +0200

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    num_args = len(argv) - 1
+    args_str = "argument" if num_args == 1 else "arguments"
+
+    print("{} {}:".format(num_args, args_str))
+
+    for i in range(1, num_args + 1):
+        print("{}: {}".format(i, argv[i]))
+
+    if num_args == 0:
+        print(".", end="")
+    print()
