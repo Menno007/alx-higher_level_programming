fbbc6075268bf287db950b282e00961b07f29e63
Menno007 <Menna.soft3@gmail.com>
2023-12-01 04:34:11 +0200

    5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -6,3 +6,10 @@ if __name__ == "__main__":
     for name in names:
         if name[:2] != "__":
             print(name)
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
