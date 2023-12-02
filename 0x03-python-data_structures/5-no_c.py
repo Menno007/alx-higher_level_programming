66048a50179a7cf93086ff81df80e8c5fe9cbd12
Menno007 <Menna.soft3@gmail.com>
2023-12-02 19:31:49 +0200

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-  
+    copy_string = ""
+    for x in my_string:
+        if x != 'c' and x != 'C':
+            copy_string = copy_string + x
+    return (copy_string)
