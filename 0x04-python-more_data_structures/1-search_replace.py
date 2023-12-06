cdacd581e90dab3a52c180b3a87ca411da700000
Menno007 <Menna.soft3@gmail.com>
2023-12-06 23:00:41 +0200

    1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
+    new_list = [replace if element == search else element for element in my_list]
     return new_list
