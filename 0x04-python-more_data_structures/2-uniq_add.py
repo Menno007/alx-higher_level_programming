48d95d7b13c6b84a07aec712bbb4bcfdfd9ab81f
Menno007 <Menna.soft3@gmail.com>
2023-12-07 00:14:21 +0200

    2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -7,3 +7,16 @@ def uniq_add(my_list=[]):
             uniq_list.append(i)
             sum = sum + i
     return (sum)
+
+
+"""
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += i
+
+    return (num)
+"""
