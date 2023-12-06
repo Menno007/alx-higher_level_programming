e07136ae783fb5742192ea9fd56bb08f800d5354
Menno007 <Menna.soft3@gmail.com>
2023-12-06 19:11:46 +0200

    0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    sqr = matrix.copy()
     for i in range(len(matrix)):
-        sqr = list(map(lambda x: x ** 2, matrix[i]))
+        sqr[i] = list(map(lambda x: x ** 2, matrix[i]))
     return sqr
