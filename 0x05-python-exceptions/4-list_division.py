8467ac118deec77ee2de83e74c7f43ce145ca800
Menno007 <Menna.soft3@gmail.com>
2023-12-19 02:19:48 +0200

    4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,7 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = element_1 / element_2
         except ZeroDivisionError:
             print("division by 0")
-        except (ValueError, TypeError):
+        except TypeError:
             print("wrong type")
         except IndexError:
             print("out of range")
